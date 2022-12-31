2ba8b206ff193bb8d17023dc19ce4cb7a90f3d6d
Pablo Curty <pablo.curty@visagio.com>
2022-12-31 15:59:04 -0300

    bcryptjs hash to encrypt password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,43 +1,46 @@
 import User from "../models/User";
+import { createPasswordHash } from "../services/Auth";
 
 class UsersController {
-    async index(req, res) {
-        try {
-            const users = await User.find();
-            return res.json(users);
-        } catch (err) {
-            console.error(err);
-            return res.status(500).json({error: "Internal server error."})
-        }
+  async index(req, res) {
+    try {
+      const users = await User.find();
+      return res.json(users);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error." });
     }
+  }
 
-    async show(req, res) {
+  async show(req, res) {}
 
+  async create(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(422)
+          .json({ message: `User ${email} already exists.` });
+      }
+      //criptografa passaword
+      const encryptedPassword = await createPasswordHash(password);
+        
+      const newUser = await User.create({
+        email,
+        password: encryptedPassword,
+      });
+
+      return res.status(201).json(newUser);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal server error." });
     }
+  }
 
-    async create(req, res) {
-        try {
-            const { email, password } = req.body;
-            const user = await User.findOne({ email });
-            if (user) {
-                return res.status(422).json({ message: `User ${email} already exists.` });
-            }
-            const newUser = await User.create({ email, password });
-            return res.status(201).json(newUser);
-        } catch (err) {
-            console.error(err);
-            return res.status(500).json({ error: "Internal server error." });
-        }
+  async update(req, res) {}
 
-    }
-
-    async update(req, res) {
-
-    }
-
-    async destroy(req, res) {
-
-    }
+  async destroy(req, res) {}
 }
 
 export default new UsersController();
